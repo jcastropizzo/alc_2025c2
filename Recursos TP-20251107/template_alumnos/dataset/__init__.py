b5693f20aacdf6fc5bd9e70bed3a8e007229c37e
Francisco Sandalinas <francisco.sandalinas@compass.com>
2025-11-10 23:05:52 -0300

    Que gil baba tenia razon

--- Recursos TP-20251107/template_alumnos/dataset/__init__.py
+++ Recursos TP-20251107/template_alumnos/dataset/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from numpy import (
-    load, ndarray, concatenate, zeros, ones, column_stack
+    load, ndarray, concatenate, zeros, ones, column_stack, array
 )
 
 CLASSES = ['cats', 'dogs']
@@ -12,8 +12,8 @@ def cargarDataset(file: Path) -> Datos:
     train_path = file / 'train'
     paths = [train_path / cls / EMBEDDING_NAME for cls in CLASSES]
     training_data = concatenate([load(path) for path in paths], axis=1)
-    label_data = column_stack([ones(training_data.shape[1] // 2), zeros(training_data.shape[1] // 2)])
-    label_data = concatenate([label_data, column_stack([zeros(training_data.shape[1] // 2), ones(training_data.shape[1] // 2)])], axis=0)
+    label_data = array([[1, 0]] * (training_data.shape[1] // 2))
+    label_data = concatenate([label_data, array([[0, 1]] * (training_data.shape[1] // 2))], axis=0)
 
     assert label_data.shape[0] == training_data.shape[1]
     assert training_data.shape[0] == 1536
