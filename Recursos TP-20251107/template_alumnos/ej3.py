047fd3db4a4bfe316f656cad4ef131c033543d94
Bruno Bruzzo <bruzzo.bruno@gmail.com>
2025-11-14 20:33:19 -0300

    validation on ej3

--- Recursos TP-20251107/template_alumnos/ej3.py
+++ Recursos TP-20251107/template_alumnos/ej3.py
@@ -11,16 +11,15 @@ def test():
 def pinvSVD(U, S, V, Y,imp='alc'):
     n = U.shape[0]
     p = V.shape[0]
-    print("n: ", n)
-    print("p :", p)
-    print("p - n: ", p-n)
 
-    if(imp == 'np'):
+    if imp == 'np':
+        print("Inside pinSVD, I entered np option...")
         Ut = transpuesta(U)
         Vt = transpuesta(V) # En realidad SVD devuelve V transpuesta, por lo cual usamos V en la inversa de X. Mantengo naming conventions del enunciado.
         V1 = Vt[:,0:n]
+        S = np.linalg.pinv(S)
         return Y.T@V1@S@Ut
-    elif(imp == 'alc'):
+    elif imp == 'alc':
         #implement alc version
         return 1
 
@@ -31,26 +30,27 @@ def alc_imp():
 
 def np_imp():
     X_train, Y_train, X_val, Y_val = cargarDataset(Path("./dataset/cats_and_dogs"))
-    print(Y_train)
-    print(Y_val)
+
     U, srow, V = np.linalg.svd(X_train)
     S = np.diag(srow)
-    S = 1/S
     W = pinvSVD(U, S, V, Y_train,'np')
-    
-    print("xval shape: ",X_val.shape)
-    print("yval shape: ",Y_val.shape)
-    print("ytrain shape: ", Y_train.shape)
-    print("xval",X_val)
-    print("xtrain", X_train)
 
-    for i in range(X_val.shape[1]):
-        print("inside forloop")
-        print("X shape", X_val.shape)
-        print("W shape", W.shape)
-        X_val[:,i]@W
+    predicciones_correctas = 0
+    samples = X_val.shape[1]
+    for i in range(samples):
+        y_true_vector = Y_val[i, :]
+        ei = W@X_val[:,i]
+        predicted_class = np.argmax(ei)
+        true_class = np.argmax(y_true_vector)
+        if predicted_class == true_class:
+            predicciones_correctas += 1
+        #print("iteration", i, ":",ei)
+        #print("iteration", i, " Y",Y_val[i,:])
 
-
+    accuracy = (predicciones_correctas / samples) * 100
+    print(f"\n--- Resultados de Validación ---")
+    print(f"Precisión (Accuracy): {accuracy:.2f}%")
+    print(f"Clasificó correctamente {predicciones_correctas} de {samples} muestras.")
     return 1
         
 
